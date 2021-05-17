8e1c638150d4bd0b8a5421d18e8fc255394be670
erolaksoy <erolaksoy98@gmail.com>
2021-05-17 19:09:27 +0300

    add request permission operation

--- app/src/main/java/com/example/android_permissions/MainActivity.kt
+++ app/src/main/java/com/example/android_permissions/MainActivity.kt
@@ -1,11 +1,66 @@
 package com.example.android_permissions
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var imageView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        imageView = findViewById(com.example.android_permissions.R.id.imageView)
+
+        imageView.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermissions(
+                    arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                    REQUEST_CODE_PERMISSION
+                )
+            } else {
+                showGallery()
+            }
+        }
+    }
+
+    private fun showGallery() {
+        Intent(Intent.ACTION_PICK).also { intent ->
+            intent.type = "image/*"
+            startActivityForResult(intent, REQUEST_CODE_IMAGE_PICK)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_CODE_PERMISSION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) showGallery()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_IMAGE_PICK && resultCode == Activity.RESULT_OK) {
+            val virtualFileUri = data!!.data
+            imageView.setImageURI(virtualFileUri)
+        }
+    }
+
+    companion object {
+        const val REQUEST_CODE_IMAGE_PICK = 101
+        const val REQUEST_CODE_PERMISSION = 102
     }
 }
